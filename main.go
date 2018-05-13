db6b20590e8964e3d7d7d699ed6575aa05d36a8e
klassjin <skjin@Shikangs-MacBook-Pro.local>
2018-05-13 15:00:36 -0700

    update IP

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ const (
 	//PROJECT_ID = "around-xxx"
 	//BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://35.190.154.54:9200"
+	ES_URL = "http://35.196.192.233:9200"
 )
 
 
